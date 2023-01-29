54da033a08fcbd23037157072d5e15a47e05374b
zabooz <zabooz1988@gmail.com>
2023-01-29 08:33:09 +0100

    finished

---
+++ script.js
@@ -0,0 +1,23 @@
+const menuBtns = document.querySelectorAll('.toggle');
+const navBar = document.querySelector('nav')
+const mainImage = document.querySelector('main > img')
+const screenWidth = document.querySelector('body');
+
+
+
+menuBtns.forEach(item =>{
+    item.addEventListener('click', e=>{
+        navBar.classList.toggle('nav-show')
+    })
+});
+
+imageChange = () =>{
+    if(window.innerWidth >= 800){
+        mainImage.src = '/assets/images/image-web-3-desktop.jpg'
+    }else{
+        mainImage.src = '/assets/images/image-web-3-mobile.jpg'
+    }
+}
+imageChange()
+
+
